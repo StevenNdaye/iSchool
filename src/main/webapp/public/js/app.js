9fdf729f1c16f03c5a0ea7fc6d5f95310bd802c9
StevenNdaye <stevenndaye@gmail.com>
2016-03-12 15:09:17 +0200

    Steven: branching to a new design

---
+++ src/main/webapp/public/js/app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var highSchool = angular
+    .module('highSchool', [
+        'ngRoute',
+        'ui.bootstrap',
+        'controllers'
+    ]).config(['$routeProvider', '$locationProvider',
+        function ($routeProvider, $locationProvider) {
+            $locationProvider.html5Mode(true);
+
+            $routeProvider.
+            when('/', {
+                templateUrl: '/partials/_index.html',
+                controller: 'IndexCtrl'
+            }).
+            //when('/bikes', {
+            //    templateUrl: '/partials/_bikes.html',
+            //    controller: 'BikesCtrl'
+            //}).
+            //when('/milages', {
+            //    templateUrl: '/partials/_milages.html',
+            //    controller: 'MilagesCtrl'
+            //}).
+            //when('/gallery', {
+            //    templateUrl: '/partials/_gallery.html',
+            //    controller: 'GalleryCtrl'
+            //}).
+            //when('/tracks', {
+            //    templateUrl: '/partials/_tracks.html',
+            //    controller: 'TracksCtrl'
+            //}).
+            //when('/tracks/:id', {
+            //    templateUrl: '/partials/_track.html',
+            //    controller: 'TrackCtrl'
+            //}).
+            //when('/location', {
+            //    templateUrl: '/partials/_location.html',
+            //    controller: 'LocationCtrl'
+            //}).
+            //when('/about', {
+            //    templateUrl: '/partials/_about.html',
+            //    controller: 'AboutCtrl'
+            //}).
+            otherwise({
+                redirectTo: '/'
+            });
+        }
+    ]);
---
+++ src/main/webapp/public/js/controllers.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var controllers = angular
+    .module('controllers', []);
+
+controllers.controller('IndexCtrl', ['$scope', '$http', '$interval', function ($scope, $http, $interval) {
+
+}]);
