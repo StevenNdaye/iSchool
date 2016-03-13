0af760810726f9023e5ac10f0d3479e849d489a4
StevenNdaye <stevenndaye@gmail.com>
2016-03-13 20:32:50 +0200

    Steven: building the student registration form

--- src/main/webapp/public/js/controllers.js
+++ src/main/webapp/public/js/controllers.js
@@ -12,13 +12,26 @@ controllers.controller('IndexCtrl', ['$scope', '$http', '$interval', '$location'
         $location.path('/student-attendance');
     };
 
-    $scope.gotoStudentAttendanceScreen = function () {
+    $scope.gotoReportScreen = function () {
         $location.path('/reports');
     };
 }]);
 
 controllers.controller('StudentRegistrationCtrl', ['$scope', '$http', '$interval', '$location', function ($scope, $http, $interval, $location) {
+    $scope.reg_classes = ["Class one", "Class two", "Class three", "Class four", "Class five", "Class six"];
+    $scope.selectedClass = undefined;
 
+    $scope.handleSelectedItem = function (reg_class) {
+        $scope.selectedClass = reg_class;
+    };
+
+    $scope.goBack = function () {
+        $location.path('/');
+    };
+
+    $scope.register = function () {
+
+    };
 }]);
 
 controllers.controller('StudentAttendanceCtrl', ['$scope', '$http', '$interval', '$location', function ($scope, $http, $interval, $location) {
