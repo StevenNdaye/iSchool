320bbe8d6744d96d34108b41d7a82015a257a2dc
StevenNdaye <stevenndaye@gmail.com>
2016-03-23 13:47:43 +0200

    Steven: cleaned up the code base

--- src/main/java/com/datadigest/challenge/repository/ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.datadigest.challenge.repository;
-
-import com.datadigest.challenge.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Integer> {
-
-}
